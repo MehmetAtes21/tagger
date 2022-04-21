f265ff4072076e8af551b8693a210a808150d1b3
HerlockApiniz <96447407+HerlockApiniz@users.noreply.github.com>
2022-04-22 03:25:49 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,11 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.types import Message, User
 from pyrogram.types.messages_and_media import Message
 from pyrogram import Client, filters
 import time
@@ -398,6 +403,86 @@ async def duyuru(event):
     except:
       pass
   await event.respond(f"Gönderildi.")
+
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.message_id
+
+    elif not message.from_user.is_self:
+        reply_id = message.message_id
+
+    return reply_id
+
+
+infotext = (
+    "**[{full_name}](tg://user?id={user_id})**\n"
+    " * ID: `{user_id}`\n"
+    " * İlk adı: `{first_name}`\n"
+    " * Soyadı: `{last_name}`\n"
+    " * Kullanıcı adı: `{username}`\n"
+    " * Son Çevrimiçi: `{last_online}`\n"
+    " * Bio: {bio}"
+)
+
+
+def LastOnline(user: User):
+    if user.is_bot:
+        return ""
+    elif user.status == "recently":
+        return "Recently"
+    elif user.status == "within_week":
+        return "Within the last week"
+    elif user.status == "within_month":
+        return "Within the last month"
+    elif user.status == "long_time_ago":
+        return "A long time ago :("
+    elif user.status == "online":
+        return "Currently Online"
+    elif user.status == "offline":
+        return datetime.fromtimestamp(user.status.date).strftime(
+            "%a, %d %b %Y, %H:%M:%S"
+        )
+
+
+def FullName(user: User):
+    return user.first_name + " " + user.last_name if user.last_name else user.first_name
+
+
+@pbot.on_message(filters.command("info") & ~filters.edited & ~filters.bot)
+async def info(client, message):
+    cmd = message.command
+    if not message.reply_to_message and len(cmd) == 1:
+        get_user = message.from_user.id
+    elif len(cmd) == 1:
+        get_user = message.reply_to_message.from_user.id
+    elif len(cmd) > 1:
+        get_user = cmd[1]
+        try:
+            get_user = int(cmd[1])
+        except ValueError:
+            pass
+    try:
+        user = await client.get_users(get_user)
+    except PeerIdInvalid:
+        await message.reply("O Kullanıcıyı tanımıyorum.")
+        return
+    desc = await client.get_chat(get_user)
+    desc = desc.description
+    await message.reply_text(
+        infotext.format(
+            full_name=FullName(user),
+            user_id=user.id,
+            user_dc=user.dc_id,
+            first_name=user.first_name,
+            last_name=user.last_name if user.last_name else "",
+            username=user.username if user.username else "",
+            last_online=LastOnline(user),
+            bio=desc if desc else "`Biyo kurulum yok.`",
+        ),
+        disable_web_page_preview=True,
+    )
 
 
 app.run()
