a00855b15fb0bd4b7b2e8450b5ebcf79e5382b6b
HerlockApiniz <96447407+HerlockApiniz@users.noreply.github.com>
2022-05-05 04:57:25 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -292,163 +292,352 @@ async def mentionalladmin(event):
 
 
 
-@app.on_message(filters.command(["tag"], ["@"]) & ~filters.private)
-def tag(_, message: Message):
+class Database: 
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
 
-    kısa = False
+    def new_user(self, id): # Veritabanına yeni kullanıcı ekler
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason="",
+            ),
+        )
 
-    try:
-        message.text.split()[1]
-    except IndexError:
+    async def add_user(self, id): # Veritabına yeni kullanıcı eklemek için ön def
+        user = self.new_user(id)
+        await self.col.insert_one(user)
 
-        kısa = True
+    async def is_user_exist(self, id): # Bir kullanıcının veritabında olup olmadığını kontrol eder.
+        user = await self.col.find_one({"id": int(id)})
+        return bool(user)
 
-    if kısa == False:
+    async def total_users_count(self): # Veritabanındaki toplam kullanıcıları sayar.
+        count = await self.col.count_documents({})
+        return count
 
-        if message.text.split()[1].isnumeric() == True:
-            uye_sayi = int(app.get_chat_members_count(message.chat.id))
-            metin = ""
-            sayi = int(message.text.split()[1])
-            sayac = 0
-            kisiler = app.get_chat_members(message.chat.id)
+    async def get_all_users(self): # Veritabındaki tüm kullanıcıların listesini verir.
+        return self.col.find({})
 
-            if uye_sayi < sayi:
-                message.reply(
-                        "__🇹🇷 Girdiğiniz Sayı Grup Üye Sayısından Fazla !!\n\nEtiketleme İşlemi Yapılmıyor...")
+    async def delete_user(self, user_id): # Veritabından bir kullanıcıyı siler.
+        await self.col.delete_many({"id": int(user_id)})
 
+    async def ban_user(self, user_id, ban_duration, ban_reason): # Veritabanınızdaki bir kullanıcıyı yasaklılar listesine ekler.
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
+        )
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+
+    async def remove_ban(self, id): # Veritabanınızdaki yasaklılar listesinde bulunan bir kullanıcın yasağını kaldırır.
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
+    async def get_ban_status(self, id): # Bir kullanıcın veritabanınızda yasaklılar listesinde olup olmadığını kontrol eder.
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("ban_status", default)
+
+    async def get_all_banned_users(self): # Veritabınızdaki yasaklı kullanıcılar listesini verir.
+        return self.col.find({"ban_status.is_banned": True})
+
+
+db = Database(DATABASE_URL, BOT_USERNAME)
+mongo_db_veritabani = MongoClient(DATABASE_URL)
+dcmdb = mongo_db_veritabani.handlers
+
+
+
+################## KULLANICI KONTROLLERİ #############
+async def handle_user_status(bot: Client, cmd: Message): # Kullanıcı kontrolü
+    chat_id = cmd.chat.id
+    if not await db.is_user_exist(chat_id):
+        if cmd.chat.type == "private":
+            await db.add_user(chat_id)
+            await bot.send_message(LOG_CHANNEL,LAN.BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id))
+        else:
+            await db.add_user(chat_id)
+            chat = bot.get_chat(chat_id)
+            if str(chat_id).startswith("-100"):
+                new_chat_id = str(chat_id)[4:]
             else:
-                for i in message.text.split()[2:]:
-                    metin += i + " "
+                new_chat_id = str(chat_id)[1:]
+            await bot.send_message(LOG_CHANNEL,LAN.GRUP_BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id, chat.title, cmd.chat.id, cmd.chat.id, cmd.message_id))
 
-                chat_id = message.chat.id
-
-                message.reply(
-                    f"🇹🇷 {sayi} ** Kişi Etiketleniyor**...\n\n**Sebep** :  __{metin}__")
-
-                for kisi in kisiler:
-
-                    if kisi.user.is_bot == False:
-
-                        isim = kisi.user.first_name
-                        try:
-                                app.send_message(chat_id, f"\n·{isim}\n\n[{metin}](tg://user?id={kisi.user.id}) ")
-                        except:
-                            pass
-                        time.sleep(3)
-
-                        sayac += 1
-                        if sayac == sayi:
-                            app.send_message(chat_id,
-                                                 f"🇹🇷 {sayi} **Kişi Etiketlendi...**")
-                            break
-        elif message.text.split()[1].isnumeric() == False:
-
-
-            metin = ""
-            sayi = 50
-            sayac = 0
-            kisiler = app.get_chat_members(message.chat.id)
-
-
-            for i in message.text.split()[1:]:
-                metin += i + " "
-
-            print(metin)
-            message.reply(f"🇹🇷 ** Üyeler Etiketleniyor**...\n**Sebep** :  __{metin}__\n\n🇬🇧 **I'm tagging users...")
-
-            for kisi in kisiler:
-                if kisi.user.is_bot == False:
-                    isim = kisi.user.first_name
-                    try:
-                        app.send_message(message.chat.id, f"\n·{metin} [{isim}](tg://user?id={kisi.user.id}) ")
-                    except:
-                        pass
-                    time.sleep(3)
-
-                    sayac += 1
-
-                    if sayac == sayi:
-                        app.send_message(message.chat.id,
-                                         "🇹🇷 **Etiketleme bitti...**")
-                        break
-
-
-    elif kısa == True:
-
-        metin = ""
-        sayi = 50
-        sayac = 0
-        kisiler = app.get_chat_members(message.chat.id)
-
-        message.reply(f"🇹🇷 ** Üyeler Etiketleniyor**...\n**Sebep** :  __{metin}__")
-
-        for kisi in kisiler:
-            if kisi.user.is_bot == False:
-                isim = kisi.user.first_name
-                try:
-                    app.send_message(message.chat.id, f"\n·{metin} [{isim}](tg://user?id={kisi.user.id}) ")
-                except:
-                    pass
-                time.sleep(3)
-
-                sayac += 1
-                if sayac == sayi:
-                    app.send_message(message.chat.id, "🇹🇷 **Etiketleme bitti...**")
-                    break
+    ban_status = await db.get_ban_status(chat_id) # Yasaklı Kullanıcı Kontrolü
+    if ban_status["is_banned"]:
+        if int((datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])).days) > int(ban_status["ban_duration"]):
+            await db.remove_ban(chat_id)
+        else:
+            if GROUP_SUPPORT:
+                msj = f"@{GROUP_SUPPORT}"
+            else:
+                msj = f"[{LAN.SAHIBIME}](tg://user?id={OWNER_ID})"
+            if cmd.chat.type == "private":
+                await cmd.reply_text(LAN.PRIVATE_BAN.format(msj), quote=True)
+            else:
+                await cmd.reply_text(LAN.GROUP_BAN.format(msj),quote=True)
+                await bot.leave_chat(cmd.chat.id)
+            return
+    await cmd.continue_propagation()
 
 
 
 
+############### Broadcast araçları ###########
+broadcast_ids = {}
 
-@client.on(events.NewMessage())
-async def mentionalladmin(event):
-  global grup_sayi
-  if event.is_group:
-    if event.chat_id in grup_sayi:
-      pass
+
+async def send_msg(user_id, message): # Mesaj Gönderme
+    try:
+        if GONDERME_TURU is False:
+            await message.forward(chat_id=user_id)
+        elif GONDERME_TURU is True:
+            await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(int(e.x))
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id}: {LAN.NOT_ONLINE}\n"
+    except UserIsBlocked:
+        return 400, f"{user_id}: {LAN.BOT_BLOCKED}\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id}: {LAN.USER_ID_FALSE}\n"
+    except Exception:
+        return 500, f"{user_id}: {traceback.format_exc()}\n"
+
+async def main_broadcast_handler(m, db): # Ana Broadcast Mantığı
+    all_users = await db.get_all_users()
+    broadcast_msg = m.reply_to_message
+    while True:
+        broadcast_id = "".join(random.choice(string.ascii_letters) for i in range(3))
+        if not broadcast_ids.get(broadcast_id):
+            break
+    out = await m.reply_text(
+        text=LAN.BROADCAST_STARTED)
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total=total_users, current=done, failed=failed, success=success)
+    async with aiofiles.open("broadcast-logs-g4rip.txt", "w") as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id=int(user["id"]), message=broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user["id"])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(
+                    dict(current=done, failed=failed, success=success))
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    if failed == 0:
+        await m.reply_text(text=LAN.BROADCAST_STOPPED.format(completed_in, total_users, done, success, failed), quote=True,)
     else:
-      grup_sayi.append(event.chat_id)
-
-@client.on(events.NewMessage(pattern='^/statik ?(.*)'))
-async def son_durum(event):
-    global anlik_calisan,grup_sayi,ozel_list
-    sender = await event.get_sender()
-    if sender.id not in ozel_list:
-      return
-    await event.respond(f"**Bot Güncellendi✅\n@MinaTagBot Güncel Verileri 🖥️**\n\n**Toplam Grub: `{len(grup_sayi)}`\n\nAnlık Çalışan Grub: `{len(anlik_calisan)}`**")
+        await m.reply_document(document="broadcast-logs-mina.txt", caption=LAN.BROADCAST_STOPPED.format(completed_in, total_users, done, success, failed), quote=True,)
+    os.remove("broadcast-logs-mina.txt")
 
 
-@client.on(events.NewMessage(pattern='^/reklam ?(.*)'))
-async def duyuru(event):
- 
-  global grup_sayi,ozel_list
-  sender = await event.get_sender()
-  if sender.id not in ozel_list:
-    return
-  reply = await event.get_reply_message()
-  await event.respond(f"Toplam {len(grup_sayi)} Gruba'a mesaj gönderiliyor...")
-  for x in grup_sayi:
-    try:
-      await client.send_message(x,f"**📣 Sponsor**\n\n{reply.message}")
-    except:
-      pass
-  await event.respond(f"Gönderildi.")
+delcmdmdb = dcmdb.admins
 
-@client.on(events.NewMessage(pattern='^/duyuru ?(.*)'))
-async def duyuru(event):
- 
-  global grup_sayi,ozel_list
-  sender = await event.get_sender()
-  if sender.id not in ozel_list:
-    return
-  reply = await event.get_reply_message()
-  await event.respond(f"Toplam {len(grup_sayi)} Gruba'a mesaj gönderiliyor...")
-  for x in grup_sayi:
-    try:
-      await client.send_message(x,f"**📣 Duyuru**\n\n{reply.message}")
-    except:
-      pass
-  await event.respond(f"Gönderildi.")
+async def delcmd_is_on(chat_id: int) -> bool: # Grup için mesaj silme özeliğinin açık olup olmadığını kontrol eder.
+    chat = await delcmdmdb.find_one({"chat_id": chat_id})
+    return not chat
+
+
+async def delcmd_on(chat_id: int): # Grup için mesaj silme özeliğini açar.
+    already_del = await delcmd_is_on(chat_id)
+    if already_del:
+        return
+    return await delcmdmdb.delete_one({"chat_id": chat_id})
+
+
+async def delcmd_off(chat_id: int): # Grup için mesaj silme özeliğini kapatır.
+    already_del = await delcmd_is_on(chat_id)
+    if not already_del:
+        return
+    return await delcmdmdb.insert_one({"chat_id": chat_id})
+
+
+
+################# SAHİP KOMUTLARI #############
+
+# Verileri listeleme komutu
+@Client.on_message(filters.command("stats") & filters.user(OWNER_ID))
+async def botstats(bot: Client, message: Message):
+    g4rip = await bot.send_message(message.chat.id, LAN.STATS_STARTED.format(message.from_user.mention))
+    all_users = await db.get_all_users()
+    groups = 0
+    pms = 0
+    async for user in all_users:
+        if str(user["id"]).startswith("-"):
+            groups += 1
+        else:
+            pms += 1
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    total_users = await db.total_users_count()
+    await g4rip.edit(text=LAN.STATS.format(BOT_USERNAME, total_users, groups, pms, total, used, disk_usage, free, cpu_usage, ram_usage, __version__), parse_mode="md")
+
+
+
+# Botu ilk başlatan kullanıcıların kontrolünü sağlar.
+@Client.on_message()
+async def G4RIP(bot: Client, cmd: Message):
+    await handle_user_status(bot, cmd)
+
+
+
+# Broadcast komutu
+@Client.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
+async def broadcast_handler_open(_, m: Message):
+    await main_broadcast_handler(m, db)
+
+
+
+# Bir kullanıcı yasaklama komutu
+@Client.on_message(filters.command("block") & filters.user(OWNER_ID))
+async def ban(c: Client, m: Message):
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+        if len(m.command) <= 1:
+            ban_duration = 9999
+            ban_reason = LAN.BAN_REASON.format(BOT_USERNAME)
+        elif len(m.command) == 2:
+            ban_duration = 9999
+            ban_reason = " ".join(m.command[1:])
+    else:
+        if len(m.command) <= 1:
+            return await m.reply(LAN.NEED_USER)
+        elif len(m.command) == 2:
+            user_id = int(m.command[1])
+            ban_duration = 9999
+            ban_reason = LAN.BAN_REASON.format(BOT_USERNAME)
+        elif len(m.command) == 3:
+            user_id = int(m.command[1])
+            ban_duration = 9999
+            ban_reason = " ".join(m.command[2:])
+    
+        if str(user_id).startswith("-"):
+            try:    
+                ban_log_text = LAN.BANNED_GROUP.format(m.from_user.mention, user_id, ban_duration, ban_reason)
+                await c.send_message(user_id, LAN.AFTER_BAN_GROUP.format(ban_reason))
+                await c.leave_chat(user_id)
+                ban_log_text += LAN.GROUP_BILGILENDIRILDI
+            except BaseException:
+                traceback.print_exc()
+                ban_log_text += LAN.GRUP_BILGILENDIRILEMEDI.format(traceback.format_exc())
+        else:
+            try:    
+                ban_log_text = LAN.USER_BANNED.format(m.from_user.mention, user_id, ban_duration, ban_reason)
+                await c.send_message(user_id, LAN.AFTER_BAN_USER.format(ban_reason))
+                ban_log_text += LAN.KULLANICI_BILGILENDIRME
+            except BaseException:
+                traceback.print_exc()
+                ban_log_text += LAN.KULLANICI_BILGILENDIRMEME.format(traceback.format_exc())
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        await c.send_message(LOG_CHANNEL, ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+
+
+
+# Bir kullanıcın yasağını kaldırmak komutu
+@Client.on_message(filters.command("unblock") & filters.user(OWNER_ID))
+async def unban(c: Client, m: Message):
+        if m.reply_to_message:
+            user_id = m.reply_to_message.from_user.id
+        else:
+            if len(m.command) <= 1:
+                return await m.reply(LAN.NEED_USER)
+            else:
+                user_id = int(m.command[1])
+        unban_log_text = LAN.UNBANNED_USER.format(m.from_user.mention, user_id)
+        if not str(user_id).startswith("-"):
+            try:
+                await c.send_message(user_id, LAN.USER_UNBAN_NOTIFY)
+                unban_log_text += LAN.KULLANICI_BILGILENDIRME
+            except BaseException:
+                traceback.print_exc()
+                unban_log_text += LAN.KULLANICI_BILGILENDIRMEME.format(traceback.format_exc())
+        await db.remove_ban(user_id)
+        await c.send_message(LOG_CHANNEL, unban_log_text)
+        await m.reply_text(unban_log_text, quote=True)
+
+
+
+# Yasaklı listesini görme komutu
+@Client.on_message(filters.command("blocklist") & filters.user(OWNER_ID))
+async def _banned_usrs(_, m: Message):
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ""
+    async for banned_user in all_banned_users:
+        user_id = banned_user["id"]
+        ban_duration = banned_user["ban_status"]["ban_duration"]
+        banned_on = banned_user["ban_status"]["banned_on"]
+        ban_reason = banned_user["ban_status"]["ban_reason"]
+        banned_usr_count += 1
+        text += LAN.BLOCKS.format(user_id, ban_duration, banned_on, ban_reason)
+    reply_text = LAN.TOTAL_BLOCK.format(banned_usr_count, text)
+    if len(reply_text) > 4096:
+        with open("banned-user-list.txt", "w") as f:
+            f.write(reply_text)
+        await m.reply_document("banned-user-list.txt", True)
+        os.remove("banned-user-list.txt")
+        return
+    await m.reply_text(reply_text, True)
+
+
+
+############## BELİRLİ GEREKLİ DEF'LER ###########
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "K", 2: "M", 3: "G", 4: "T"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
 
 class LAN(object):
 
